c2f781ba6cec688ffd7ddfda73d86601875e262e
Pratyay Biswas <biswaspratyay132@gmail.com>
2026-07-19 20:34:24 +0530

    Update build_linux.sh

--- build_linux.sh
+++ build_linux.sh
@@ -2,18 +2,14 @@
 
 set -e
 
-echo "Installing Python packages..."
+echo "Installing dependencies..."
 pip install -r requirements.txt
-
-echo "Installing PyInstaller..."
-pip install pyinstaller
 
 echo "Cleaning previous builds..."
 rm -rf build
 rm -rf dist
 
-echo "Building..."
-
+echo "Building application..."
 pyinstaller UniversalVideoDownloader.spec
 
-echo "Done!"
+echo "Build completed successfully!"
